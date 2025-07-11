29de0fe66f052a89b2677da6d600b0bffd325938
sanxgit <msanthosh2436@gmail.com>
2025-07-11 18:49:14 +0530

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -11,11 +11,11 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline. 
-
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000", "https://localhost:3000"));
 app.MapControllers();
 
 // since we cant able to access DI from where we are declaring, we are accessing the dbcontext like this
